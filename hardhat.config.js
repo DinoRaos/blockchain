5ca6bad64a8f33c102767551c0d3cd6af771969e
dino.raosavac@study.thws.de <dino.raosavac@study.thws.de>
2025-02-05 19:42:44 +0100

    initial commit project

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-waffle");
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    hardhat: {},
+    // Hier kannst du weitere Netzwerke konfigurieren (z.B. Rinkeby, Goerli etc.)
+  },
+};
